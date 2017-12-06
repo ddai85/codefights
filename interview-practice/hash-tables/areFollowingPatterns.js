174450741838f9ca2a7902d64c4846f9aa8438d8
DanDai <ddai85@gmail.com>
2017-12-06 09:07:34 -0800

    finished are following pattern toy problem

---
+++ interview-practice/hash-tables/areFollowingPatterns.js
@@ -0,0 +1,30 @@
+function areFollowingPatterns(strings, patterns) {
+  
+  const convertToNum = function(array) {
+    let index = 0;
+    let objStore = {};
+    let arrayNum = [];
+
+    for (let i = 0; i < array.length; i++) {
+      if (objStore[array[i]] !== undefined) {
+        arrayNum.push(objStore[array[i]]);
+      } else {
+        objStore[array[i]] = index;
+        arrayNum.push(objStore[array[i]]);
+        index++;
+      }
+    }
+    return arrayNum.join();
+  }
+  console.log(convertToNum(strings));
+  console.log(convertToNum(patterns));
+  if (convertToNum(strings) === convertToNum(patterns)) {
+    return true;
+  } else {
+    return false;
+  }
+}
+let strings = ["cat", "dog", "dog"];
+let patterns = ["a", "b", "b"];
+console.log(areFollowingPatterns(strings, patterns));
+  
